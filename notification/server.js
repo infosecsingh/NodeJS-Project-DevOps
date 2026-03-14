399fd7cef7f706ea7144a0ec9eed745a807b1846
Inderjeet Singh <infosecsingh@gmail.com>
2026-03-14 15:57:26 +0530

    fixing notification server for accepting backend response

--- notification/server.js
+++ notification/server.js
@@ -1,22 +1,50 @@
-const express = require("express")
-const nodemailer = require("nodemailer")
+const express = require("express");
+const nodemailer = require("nodemailer");
+require('dotenv').config(); // To use environment variables
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.post("/send",(req,res)=>{
+// Create a transporter object using the default SMTP transport
+// You will need to set the following environment variables:
+// EMAIL_HOST, EMAIL_PORT, EMAIL_USER, EMAIL_PASS
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
 
-const {name,email,message} = req.body
 
-console.log("New hiring request")
+app.post("/send", async (req, res) => { // made the function async
+    const { name, email, message } = req.body;
 
-console.log(name,email,message)
+    console.log("New hiring request");
+    console.log(name, email, message);
 
-res.send("notification sent")
+    try {
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+            from: `"${name}" <${email}>`, // sender address
+            to: process.env.EMAIL_USER, // list of receivers
+            subject: "New Hiring Request", // Subject line
+            text: message, // plain text body
+            html: `<b>From:</b> ${name} <br/><b>Email:</b> ${email} <br/><b>Message:</b><br/>${message}`, // html body
+        });
 
-})
+        console.log("Message sent: %s", info.messageId);
+        res.send("notification sent");
 
-app.listen(3001,()=>{
-console.log("notification service running")
-})
+    } catch (error) {
+        console.error("Error sending email:", error);
+        res.status(500).send("Error sending notification");
+    }
+});
+
+app.listen(3001, () => {
+    console.log("notification service running");
+});
